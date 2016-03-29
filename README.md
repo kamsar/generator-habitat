fd3d922a215b3190c152e0d1d382c65472d1e606
Kam Figy <kamsar@gmail.com>
2016-03-28 20:30:06 -0700

    Initial commit
    Feature is partially implemented

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-habitat [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> Yeoman generator for Sitecore Habitat
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-habitat using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-habitat
+```
+
+Then generate your new project:
+
+```bash
+yo habitat
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+Apache-2.0 © [Sitecore]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-habitat.svg
+[npm-url]: https://npmjs.org/package/generator-habitat
+[travis-image]: https://travis-ci.org/Sitecore/generator-habitat.svg?branch=master
+[travis-url]: https://travis-ci.org/Sitecore/generator-habitat
+[daviddm-image]: https://david-dm.org/Sitecore/generator-habitat.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/Sitecore/generator-habitat
---
+++ generators/feature/index.js
@@ -0,0 +1,114 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var guid = require('node-uuid');
+var path = require('path');
+
+module.exports = yeoman.Base.extend({
+
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the slick ' + chalk.red('Sitecore Habitat Feature') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'input',
+      name: 'featureTitle',
+      message: 'Enter the name of your feature:'
+    },
+    {
+      type: 'confirm',
+      name: 'createTests',
+      message: 'Create unit test project (DO IT!):',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      this.props.projectGuid = '{' + guid.v4() + '}';
+      this.props.testProjectGuid = '{' + guid.v4() + '}';
+
+      done();
+    }.bind(this));
+  },
+
+  writing: function () {
+    var targetPath = path.join('src', 'Feature', this.props.featureTitle);
+
+    /*********** CODE ***************/
+    this.fs.copy(
+      this.templatePath('code/**/*'),
+      this.destinationPath(path.join(targetPath, 'code'))
+    );
+
+    // csproj
+    this.fs.copyTpl(
+      this.templatePath('Sitecore.Feature.csproj'),
+      this.destinationPath(path.join(targetPath, 'code', 'Sitecore.Feature.' + this.props.featureTitle + '.csproj')),
+      this.props
+    );
+
+    // AssemblyInfo.cs, project
+    this.fs.copyTpl(
+      this.templatePath('AssemblyInfo.cs'),
+      this.destinationPath(path.join(targetPath, 'code', 'Properties', 'AssemblyInfo.cs')),
+      { assemblyName: 'Sitecore.Feature.' + this.props.featureTitle }
+    );
+
+    // config
+    this.fs.copyTpl(
+      this.templatePath('Feature.config'),
+      this.destinationPath(path.join(targetPath, 'code', 'App_Config', 'Include', 'Feature', 'Feature.' + this.props.featureTitle + '.config')),
+      this.props
+    );
+
+    // serialization config
+    this.fs.copyTpl(
+      this.templatePath('Feature.Serialization.config'),
+      this.destinationPath(path.join(targetPath, 'code', 'App_Config', 'Include', 'Feature', 'Feature.' + this.props.featureTitle + '.Serialization.config')),
+      this.props
+    );
+
+    /*********** TESTS ***************/
+
+    if(this.props.createTests) {
+      // tests csproj
+      this.fs.copyTpl(
+        this.templatePath('Sitecore.Feature.Tests.csproj'),
+        this.destinationPath(path.join(targetPath, 'tests', 'Sitecore.Feature.' + this.props.featureTitle + '.Tests.csproj')),
+        this.props
+      );
+
+      // AssemblyInfo.cs, tests
+      this.fs.copyTpl(
+        this.templatePath('AssemblyInfo.cs'),
+        this.destinationPath(path.join(targetPath, 'tests', 'Properties', 'AssemblyInfo.cs')),
+        { assemblyName: 'Sitecore.Feature.' + this.props.featureTitle }
+      );
+    }
+
+    /*********** SERIALIZATION ***************/
+
+    // renderings YML
+    this.fs.copyTpl(
+      this.templatePath('Renderings.yml'),
+      this.destinationPath(path.join(targetPath, 'serialization', 'Renderings', this.props.featureTitle + '.yml')),
+      this.props
+    );
+
+    // templates YML
+    this.fs.copyTpl(
+      this.templatePath('Templates.yml'),
+      this.destinationPath(path.join(targetPath, 'serialization', 'Templates', this.props.featureTitle + '.yml')),
+      this.props
+    );
+  },
+
+  install: function () {
+    // sln?
+  }
+});
